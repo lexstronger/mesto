e47596a73f8c11b72508af18a179b993e66c3ed5
Alex Vysochin <stronger00@mail.ru>
2022-12-13 15:26:45 +0300

    Исправил замечания ревьюера и отправляю на вторую итерацию

--- scripts/index.js
+++ scripts/index.js
@@ -5,24 +5,26 @@ let saveButton = document.querySelector('.popup__button');
 let formPopup = document.querySelector('.popup__form');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
-let inputName = formPopup.querySelector('.popup__input_type-name');
-let inputDescription = formPopup.querySelector('.popup__input_type-description')
-
-function handleFormSubmit(event) {
-  event.preventDefault();
-  profileName.textContent = inputName.value;
-  profileDescription.textContent = inputDescription.value;
-}
+let inputName = formPopup.querySelector('.popup__input_type_name');
+let inputDescription = formPopup.querySelector('.popup__input_type_description')
 
 function clickInfoButton() {
   infoPopup.classList.add('popup_opened');
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
 };
 
 function clickCloseButton() {
   infoPopup.classList.remove('popup_opened');
 };
 
+function handleFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;  
+  clickCloseButton();
+}
+
 infoButton.addEventListener('click', clickInfoButton);
 closeButton.addEventListener('click', clickCloseButton);
 formPopup.addEventListener('submit', handleFormSubmit);
-saveButton.addEventListener('click', clickCloseButton);
